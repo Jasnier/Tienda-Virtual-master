663936940aa60a565d5feeb0a801dae901910d3a
Jasnier.P <prueba@gmail.com>
2023-05-20 16:46:06 -0500

    Initial commit

---
+++ Tienda-Virtual-master/header.php
@@ -0,0 +1,40 @@
+     <div class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 col-xs-12">
+                    <nav class="user-menu">
+                        <ul>
+                            
+                          
+                                <li><a href="login.php"><i class="fa fa-sign-in"></i>Iniciar Sesión</a></li>
+                                <li><a href="registro.php"><i class="fa fa-user"></i> Registrarse</a></li>
+                         
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-sm-4 col-xs-12">
+                    <div class="header-right">
+                        <ul class="list-unstyled list-inline">
+                            <li class="dropdown dropdown-small">
+                                <a href="admin/index.php" target="_blank"><i class="fa fa-user-secret" aria-hidden="true"></i> Administrador</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End header area -->
+    
+    <div class="site-branding-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="logo">
+                        <center>
+                            <h1><a href="index.php"><img src="img/logo-fruteria.png" alt="Logo Frutería Bilbao 610" class="img-responsive"></a></h1>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> <!-- End site branding area -->
